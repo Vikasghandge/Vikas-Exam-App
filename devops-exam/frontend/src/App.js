7251db51621060a925e57f467655112d9f20d0b4
Vikas Ghandge <ghandgevikas804@gmail.com>
2025-05-31 18:40:49 +0530

    Update App.js

--- devops-exam/frontend/src/App.js
+++ devops-exam/frontend/src/App.js
@@ -1,1 +1,15 @@
-// App.js placeholder
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Registration from './components/Registration';
+import Exam from './components/Exam';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Registration />} />
+        <Route path="/exam" element={<Exam />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
